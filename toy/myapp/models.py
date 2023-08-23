18839b28b2f3d5184439221824c07f2168d45406
notail_ <99523544+ndn1665@users.noreply.github.com>
2023-08-23 16:32:04 +0900

    Delete toy directory

--- toy/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-# Create your models here.
-
-class Info(models.Model):
-    peoplenums = models.CharField(max_length=10,default='')
-    jobs = models.CharField(max_length=10,default='')
-    locations = models.CharField(max_length=10,default='')
-    ages = models.CharField(max_length=10,default='')
--- toy/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Info
-
-class InfoSerializers(serializers.ModelSerializer):
-    class each:
-        model = Info
-        fields = ('id', 'peoplenums','jobs','locations','ages')
